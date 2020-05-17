60d9c902deea8a9faa0ce861076b74204c787c91
yash0412 <yashagar0412@gmail.com>
2020-05-17 18:42:37 +0530

    Removed folder module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,11 +6,6 @@ const routes: Routes = [
     path: "",
     redirectTo: "folder/Inbox",
     pathMatch: "full"
-  },
-  {
-    path: "folder/:id",
-    loadChildren: () =>
-      import("./folder/folder.module").then(m => m.FolderPageModule)
   }
 ];
 
