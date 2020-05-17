ff755b82fc2f7226c0fe7216bca14c8c27009001
yash0412 <yashagar0412@gmail.com>
2020-05-17 14:54:43 +0530

    Prettier Formatting

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,15 +1,16 @@
-import { NgModule } from '@angular/core';
-import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { PreloadAllModules, RouterModule, Routes } from "@angular/router";
 
 const routes: Routes = [
   {
-    path: '',
-    redirectTo: 'folder/Inbox',
-    pathMatch: 'full'
+    path: "",
+    redirectTo: "folder/Inbox",
+    pathMatch: "full"
   },
   {
-    path: 'folder/:id',
-    loadChildren: () => import('./folder/folder.module').then( m => m.FolderPageModule)
+    path: "folder/:id",
+    loadChildren: () =>
+      import("./folder/folder.module").then(m => m.FolderPageModule)
   }
 ];
 
